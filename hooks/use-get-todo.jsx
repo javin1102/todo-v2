a87635e27b068e4d4ce75ed7a0d644674ef688c4
javin1102 <javinrio112@gmail.com>
2021-10-10 23:35:37 +0700

    update headers

--- hooks/use-get-todo.jsx
+++ hooks/use-get-todo.jsx
@@ -18,6 +18,10 @@ export const useGetTodo = () => {
         headers: {
           "Content-Type": "application/json",
           "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true,
+          "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          "Access-Control-Allow-Headers":
+            "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
         },
       });
 
